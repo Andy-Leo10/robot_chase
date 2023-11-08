8358cf14e16c292d5ce0cebd27b6f8fe7866a0ac
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-11-08 16:28:42 -0500

    add cmd_vel_topic parameter

--- src/robot_chase.cpp
+++ src/robot_chase.cpp
@@ -14,11 +14,13 @@
 #include <math.h>
 #include <geometry_msgs/msg/twist.hpp>
 
-class RobotChase : public rclcpp::Node {
+class RobotChase : public rclcpp::Node
+{
 public:
-  RobotChase(const std::string &origin_frame, const std::string &destiny_frame)
+  RobotChase(const std::string &origin_frame, const std::string &destiny_frame, const std::string &cmd_vel_topic = "rick/cmd_vel")
       : Node("robot_chase"), TIMER_PERIOD_(0.05), MAX_LINEAR_SPEED_(0.5),
-        MAX_ANGULAR_SPEED_(0.5), kp_distance_(0.5), kp_yaw_(0.5) {
+        MAX_ANGULAR_SPEED_(0.5), kp_distance_(0.5), kp_yaw_(0.5)
+  {
     // initialize the tf2 listener
     auto clock = this->get_clock();
     tf_buffer_ = std::make_shared<tf2_ros::Buffer>(clock);
@@ -30,8 +32,9 @@ public:
     timer_ = create_wall_timer(std::chrono::milliseconds(timer_period_ms),
                                std::bind(&RobotChase::timer_callback, this));
     // publisher to cmd_vel of hunter
+    cmd_vel_topic_ = cmd_vel_topic;
     publisher_ =
-        this->create_publisher<geometry_msgs::msg::Twist>("rick/cmd_vel", 10);
+        this->create_publisher<geometry_msgs::msg::Twist>(cmd_vel_topic_, 10);
   }
 
 private:
@@ -48,12 +51,14 @@ private:
   geometry_msgs::msg::Twist pub_msg_;
   const float MAX_LINEAR_SPEED_;
   const float MAX_ANGULAR_SPEED_;
+  std::string cmd_vel_topic_;
   // control variables
   float error_distance_, error_yaw_, error_direction_;
   const float kp_distance_;
   const float kp_yaw_;
 
-  void timer_callback() {
+  void timer_callback()
+  {
     // 1st get the transform
     geometry_msgs::msg::Vector3 translation;
     geometry_msgs::msg::Quaternion rotation;
@@ -64,12 +69,13 @@ private:
     error_yaw_ = calculate_tf_yaw(rotation, true, false);
     error_direction_ = calculate_tf_direction(translation, false, true);
 
-    //3rd perform the movement
+    // 3rd perform the movement
     control_algorithm();
   }
 
   double calculate_tf_yaw(geometry_msgs::msg::Quaternion rotation,
-                          bool in_degree = true, bool debug = false) {
+                          bool in_degree = true, bool debug = false)
+  {
     tf2::Quaternion tf2_rotation;
     tf2_rotation.setX(rotation.x);
     tf2_rotation.setY(rotation.y);
@@ -78,19 +84,23 @@ private:
     tf2::Matrix3x3 m(tf2_rotation);
     double roll, pitch, yaw;
     m.getRPY(roll, pitch, yaw);
-    if (in_degree) {
+    if (in_degree)
+    {
       yaw = yaw * 180 / M_PI;
     }
-    if (debug) {
+    if (debug)
+    {
       RCLCPP_INFO(this->get_logger(), "yaw angle between frames: %.2f", yaw);
     }
     return yaw;
   }
 
   double calculate_tf_distance(geometry_msgs::msg::Vector3 translation,
-                               bool debug = false) {
+                               bool debug = false)
+  {
     float distance = sqrt(pow(translation.x, 2) + pow(translation.y, 2));
-    if (debug) {
+    if (debug)
+    {
       RCLCPP_INFO(this->get_logger(), "distance between frames: %.2f",
                   distance);
     }
@@ -98,12 +108,15 @@ private:
   }
 
   double calculate_tf_direction(geometry_msgs::msg::Vector3 translation,
-                                bool in_degree = true, bool debug = false) {
+                                bool in_degree = true, bool debug = false)
+  {
     double direction = atan2(translation.y, translation.x);
-    if (in_degree) {
+    if (in_degree)
+    {
       direction = direction * 180 / M_PI;
     }
-    if (debug) {
+    if (debug)
+    {
       RCLCPP_INFO(this->get_logger(), "direction between frames: %.2f",
                   direction);
     }
@@ -111,17 +124,21 @@ private:
   }
 
   std::tuple<geometry_msgs::msg::Vector3, geometry_msgs::msg::Quaternion>
-  get_transform() {
+  get_transform()
+  {
     geometry_msgs::msg::TransformStamped transformStamped;
     geometry_msgs::msg::Vector3 translation;
     geometry_msgs::msg::Quaternion rotation;
 
-    try {
+    try
+    {
       transformStamped = tf_buffer_->lookupTransform(
           origin_frame_, destiny_frame_, tf2::TimePointZero);
       translation = transformStamped.transform.translation;
       rotation = transformStamped.transform.rotation;
-    } catch (tf2::TransformException &ex) {
+    }
+    catch (tf2::TransformException &ex)
+    {
       RCLCPP_WARN(this->get_logger(), "Could not transform %s to %s: %s",
                   origin_frame_.c_str(), destiny_frame_.c_str(), ex.what());
     }
@@ -130,33 +147,41 @@ private:
   }
 
   template <typename T>
-  T saturate(T var, T min, T max) {
-    if (var > max) {
+  T saturate(T var, T min, T max)
+  {
+    if (var > max)
+    {
       return max;
-    } else if (var < min) {
+    }
+    else if (var < min)
+    {
       return min;
-    } else {
+    }
+    else
+    {
       return var;
     }
   }
 
-  void control_algorithm() {
-    /*orientation control: 
+  void control_algorithm()
+  {
+    /*orientation control:
     -control variable : direction defined by the vector between the two robots
     -desired variable : values close to 0
     */
     pub_msg_.angular.z = saturate(kp_yaw_ * error_direction_, -MAX_ANGULAR_SPEED_, MAX_ANGULAR_SPEED_);
-    //distance control
+    // distance control
     pub_msg_.linear.x = saturate(kp_distance_ * error_distance_, -MAX_LINEAR_SPEED_, MAX_LINEAR_SPEED_);
-    //publish the message
+    // publish the message
     publisher_->publish(pub_msg_);
   }
 };
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   rclcpp::init(argc, argv);
   rclcpp::spin(
-      std::make_shared<RobotChase>("rick/base_link", "morty/base_link"));
+      std::make_shared<RobotChase>("rick/base_link", "morty/base_link", "rick/cmd_vel"));
   rclcpp::shutdown();
   return 0;
 }
