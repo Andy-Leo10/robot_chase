365d66c8bc665e83bf29e36cf62f00d49030e8fc
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-11-07 19:24:00 -0500

    code for error between frames

--- src/robot_chase.cpp
+++ src/robot_chase.cpp
@@ -1,6 +1,7 @@
 /* ROS2 Humble
 -Create a Node that gets the latest transform between rick/base_link and morty/base_link
 -validate the transform by printing it to the screen with RCLCPP_INFO
+-Calculate the distance between the two robots and the angle between them
 */
 
 #include <rclcpp/rclcpp.hpp>
@@ -30,9 +31,11 @@ private:
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
     const float TIMER_PERIOD_;
     int timer_period_ms;
+    float error_distance_, error_yaw_, error_direction_;
     
     void timer_callback()
     {
+        //1st get the transform
         geometry_msgs::msg::TransformStamped transformStamped;
         geometry_msgs::msg::Vector3 translation;
         geometry_msgs::msg::Quaternion rotation;
@@ -48,12 +51,15 @@ private:
             RCLCPP_WARN(this->get_logger(), "Could not transform %s to %s: %s", "morty/base_link", "rick/base_link", ex.what());
             return;
         }
-        //print [x,y,theta°]
-        auto theta = calculate_theta(rotation);
-        RCLCPP_INFO(this->get_logger(), "x: %f, y: %f, theta: %f", translation.x, translation.y, theta);
+
+        //2nd calculate the distance and angle between the two robots
+        error_distance_ = calculate_tf_distance(translation,true);
+        error_yaw_ = calculate_tf_yaw(rotation,true,true);
+        error_direction_ = calculate_tf_direction(translation,true,true);
+
     }
 
-    double calculate_theta(geometry_msgs::msg::Quaternion rotation, bool in_degree = true)
+    double calculate_tf_yaw(geometry_msgs::msg::Quaternion rotation, bool in_degree = true, bool debug=false)
     {
         tf2::Quaternion tf2_rotation;
         tf2_rotation.setX(rotation.x);
@@ -67,7 +73,35 @@ private:
         {
             yaw=yaw * 180 / M_PI;
         }
+        if (debug)
+        {
+            RCLCPP_INFO(this->get_logger(), "yaw angle between frames: %.2f", yaw);
+        }
         return yaw;
+    }
+
+    double calculate_tf_distance(geometry_msgs::msg::Vector3 translation, bool debug=false)
+    {
+        float distance = sqrt(pow(translation.x, 2) + pow(translation.y, 2));
+        if (debug)
+        {
+            RCLCPP_INFO(this->get_logger(), "distance between frames: %.2f", distance);
+        }
+        return distance;
+    }
+
+    double calculate_tf_direction(geometry_msgs::msg::Vector3 translation, bool in_degree = true, bool debug=false)
+    {
+        double direction = atan2(translation.y, translation.x);
+        if (in_degree)
+        {
+            direction=direction * 180 / M_PI;
+        }
+        if (debug)
+        {
+            RCLCPP_INFO(this->get_logger(), "direction between frames: %.2f", direction);
+        }
+        return direction;
     }
 };
 
